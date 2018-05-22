502e0fdf1d2ab53bbf2c9e4c3c7cac26f82bc735
RaniNiveda <raniniveda.aug31@gmail.com>
2018-05-22 08:47:21 +0530

    user model extended

--- Myapp/models.py
+++ Myapp/models.py
@@ -1,3 +1,12 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, related_name='user', on_delete= models.CASCADE)
+    address = models.TextField(default='',blank=True)	
+    pincode = models.CharField(max_length=6,blank=True)
+    mobile = models.CharField(max_length=10,blank=True)
+    birth_date = models.DateField()
+
+
